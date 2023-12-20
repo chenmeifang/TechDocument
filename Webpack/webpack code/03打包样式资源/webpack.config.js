0c7e37ff1955863f69e82ae9018f3bdfae396256
unknown <2987642862@qq.com>
2023-12-20 22:23:28 +0800

    webpack打包样式资源

---
+++ Webpack/webpack code/03打包样式资源/webpack.config.js
@@ -0,0 +1,21 @@
+// 所有的构建工具都是基于nodejs平台运行的，模块化默认采用commonjs
+// import { resolve } from 'path'; 写法错误
+const { resolve } = require('path')
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'build.js',
+        path: resolve(__dirname, 'build')
+    },
+    // loader: [], 写法错误
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [],
+    mode: 'development'
+}
