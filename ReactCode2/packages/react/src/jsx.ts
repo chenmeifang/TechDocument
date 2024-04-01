22852c6b99bb811d6ef12908c6ba57cb8f2dc584
unknown <2987642862@qq.com>
2024-04-01 21:57:23 +0800

    react源码相关

--- ReactCode2/packages/react/src/jsx.ts
+++ ReactCode2/packages/react/src/jsx.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // 需要定义ReactElement的构造函数
 import { REACT_ELEMENT_TYPE } from 'shared/ReactSymbols';
 import {
@@ -7,24 +8,26 @@ import {
 	ReactElementType,
 	ElementType
 } from 'shared/ReactTypes';
-const ReactElement = function (type: ElementType,
+const ReactElement = function (
+	type: ElementType,
 	key: Key,
 	ref: Ref,
-	props: Props): ReactElementType {
-    const element = {
-        // 怎么区分这种数据结构是一个react element呢？
-        $$typeof: REACT_ELEMENT_TYPE,
-        key,
-        ref,
-        props,
-        type,
-        __mark: 'kasong'
-    }
-    return element;
-}
+	props: Props
+): ReactElementType {
+	const element = {
+		// 怎么区分这种数据结构是一个react element呢？
+		$$typeof: REACT_ELEMENT_TYPE,
+		key,
+		ref,
+		props,
+		type,
+		__mark: 'kasong'
+	};
+	return element;
+};
 
 export const jsx = (type: ElementType, config: any, ...maybeChildren: any) => {
-    let key: Key = null;
+	let key: Key = null;
 	const props: Props = {};
 	let ref: Ref = null;
 
@@ -42,8 +45,8 @@ export const jsx = (type: ElementType, config: any, ...maybeChildren: any) => {
 			}
 			continue;
 		}
-        // 判断是不是config自己的property，
-        // 而不是config原型上的propetry
+		// 判断是不是config自己的property，
+		// 而不是config原型上的propetry
 		if ({}.hasOwnProperty.call(config, prop)) {
 			props[prop] = val;
 		}
@@ -57,32 +60,33 @@ export const jsx = (type: ElementType, config: any, ...maybeChildren: any) => {
 		}
 	}
 	return ReactElement(type, key, ref, props);
-}
+};
 
-export const jsxDEV = jsx;
-// export const jsxDEV = (type: ElementType, config: any) => {
-// 	let key: Key = null;
-// 	const props: Props = {};
-// 	let ref: Ref = null;
+// export const jsxDEV = jsx;
+// 为什么jsxDEV不用传...maybeChildren参数
+export const jsxDEV = (type: ElementType, config: any) => {
+	let key: Key = null;
+	const props: Props = {};
+	let ref: Ref = null;
 
-// 	for (const prop in config) {
-// 		const val = config[prop];
-// 		if (prop === 'key') {
-// 			if (val !== undefined) {
-// 				key = '' + val;
-// 			}
-// 			continue;
-// 		}
-// 		if (prop === 'ref') {
-// 			if (val !== undefined) {
-// 				ref = val;
-// 			}
-// 			continue;
-// 		}
-// 		if ({}.hasOwnProperty.call(config, prop)) {
-// 			props[prop] = val;
-// 		}
-// 	}
+	for (const prop in config) {
+		const val = config[prop];
+		if (prop === 'key') {
+			if (val !== undefined) {
+				key = '' + val;
+			}
+			continue;
+		}
+		if (prop === 'ref') {
+			if (val !== undefined) {
+				ref = val;
+			}
+			continue;
+		}
+		if ({}.hasOwnProperty.call(config, prop)) {
+			props[prop] = val;
+		}
+	}
 
-// 	return ReactElement(type, key, ref, props);
-// };
+	return ReactElement(type, key, ref, props);
+};
