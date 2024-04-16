97f92c974f72f69825ef3e50706dff8555613aee
chenmeifang <2987642862@qq.com>
2024-04-16 16:11:30 +0800

    react源码

--- ReactCode2/packages/react-reconciler/src/completeWork.ts
+++ ReactCode2/packages/react-reconciler/src/completeWork.ts
@@ -7,22 +7,26 @@ import { FiberNode } from './fiber';
 import { NoFlags } from './fiberFlags';
 import { HostRoot, HostText, HostComponent } from './workTags';
 
+/**
+ * 递归中的归
+ * 创建节点真实DOM对象并将其添加到stateNode属性中
+ * @param wip 
+ * @returns 
+ */
 export const completeWork = (wip: FiberNode) => {
-	// 创建节点真实DOM对象并将其添加到stateNode属性中
-
-	// 递归中的归
-
 	const newProps = wip.pendingProps;
 	const current = wip.alternate;
 
+	// 匹配当前Fiber节点的类型（不是所有的Fiber节点都能创建其对应的DOM对象）
 	switch (wip.tag) {
+		// 普通的react元素
 		case HostComponent:
 			if (current !== null && wip.stateNode) {
 				// update
 			} else {
-				// 1. 构建DOM
+				// 1. 构建真实的DOM div span
 				const instance = createInstance(wip.type, newProps);
-				// 2. 将DOM插入到DOM树中
+				// 2. 将DOM插入到DOM树中；将所有的子级追加到父级
 				appendAllChildren(instance, wip);
 				wip.stateNode = instance;
 			}
@@ -41,7 +45,6 @@ export const completeWork = (wip: FiberNode) => {
 		case HostRoot:
 			bubbleProperties(wip);
 			return null;
-
 		default:
 			// if (__DEV__) {
 			// 	console.warn('未处理的completeWork情况', wip);
@@ -50,11 +53,18 @@ export const completeWork = (wip: FiberNode) => {
 	}
 };
 
+/**
+ * 这里的parent应该不是FiberNode，是dom节点???
+ * @param parent 
+ * @param wip 
+ * @returns 
+ */
 function appendAllChildren(parent: FiberNode, wip: FiberNode) {
 	let node = wip.child;
 
 	while (node !== null) {
 		if (node.tag === HostComponent || node.tag === HostText) {
+			// node是普通react元素或者文本
 			appendInitialChild(parent, node?.stateNode);
 		} else if (node.child !== null) {
 			node.child.return = node;
@@ -62,6 +72,9 @@ function appendAllChildren(parent: FiberNode, wip: FiberNode) {
 			continue;
 		}
 
+		// 如果node和wip是同一个节点
+		// 说明node已经退回到父级，终止循环
+		// 说明此时所有子级都已经追加到父级了
 		if (node === wip) {
 			return;
 		}
