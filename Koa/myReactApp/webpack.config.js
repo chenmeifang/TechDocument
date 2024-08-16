93a80540b658e75c3a2d6940db8e882e03f2f592
unknown <2987642862@qq.com>
2024-08-16 14:10:18 +0800

    webpack chunk

--- Koa/myReactApp/webpack.config.js
+++ Koa/myReactApp/webpack.config.js
@@ -17,10 +17,14 @@ module.exports = {
                     loader: 'babel-loader'
                 }
             },
+            // ts-loader: 是一个用于将TypeScript文件(.ts和.tsx文件)编译成JavaScript文件的Webpack加载器
+            //            主要作用是将TS代码编译为兼容的JS代码，并将编译结果传递给Webpack进行进一步的处理
             {
                 test: /\.tsx?$/,
                 use: 'ts-loader'
             },
+            // css-loader：是一个用于处理和解析CSS文件的Webpack加载器
+            //             主要作用是让你能够在JS中import或require CSS文件
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
