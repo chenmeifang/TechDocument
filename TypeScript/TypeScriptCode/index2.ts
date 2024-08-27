7467ee95628f4dc5d13d116cc868a250db80f3c6
ln <liuning202107@163.com>
2024-08-27 12:40:39 +0800

    函数重载

--- TypeScript/TypeScriptCode/index2.ts
+++ TypeScript/TypeScriptCode/index2.ts
@@ -1,1 +1,16 @@
-console.log("ts代码");
+// 函数重载
+function hello(name: string): string;
+function hello(age: number): string
+// 上面两行是函数的重载声明,告诉TS编译器hello函数可以接受两种不同的类型参数
+// 函数重载的主要意义是提供更好的类型检查和代码的自解释能力
+
+function hello (value: string | number): string {
+    if (typeof value === 'string') {
+        return '1111'
+    } else if (typeof value === 'number') {
+        return '222'
+    }
+    return '3333'
+}
+hello('111');
+hello(222);
