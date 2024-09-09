022b4a724d8777667deb06e7b81c442b36599e3a
unknown <2987642862@qq.com>
2024-09-09 20:16:17 +0800

    删除React测试项目

--- Koa/myReactApp/src/index.js
+++ Koa/myReactApp/src/index.js
@@ -7,6 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 import router from './router';
 // console.log("test:", process.env.REACT_APP_REQUEST_BASE_URL);
 // console.log('process.env.NODE_ENV:', process.env.REACT_APP_ENV);
+// console.log('222222222222222:', process.env);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App></App>
