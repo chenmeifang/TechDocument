e96a6e18800074dcdffebdda303b29a410574072
unknown <2987642862@qq.com>
2024-09-10 14:06:08 +0800

    React 调用setState时的源码流程

--- React/React code/react-test4/src/App.js
+++ React/React code/react-test4/src/App.js
@@ -2,28 +2,45 @@ import logo from './logo.svg';
 import './App.css';
 import * as React from 'react';
 
-function App() {
-  React.useEffect(() => {
-    console.log('测试钩子函数的调用');
-  }, [])
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   React.useEffect(() => {
+//     console.log('测试钩子函数的调用');
+//   }, [])
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'testname'
+    }
+  }
+  componentDidMount() {
+    this.setState({
+      name: 'chenmmeifang'
+    })
+  }
+  render() {
+    return <div>ceshi{this.state.name}</div>
+  }
 }
 
 export default App;
