b982b9704276ea2ab37b8f7553c5846a549ce491
unknown <2987642862@qq.com>
2024-10-29 22:50:18 +0800

    数组中的reduce方法

---
+++ Javascript/Code/09 数组Code/reduce.js
@@ -0,0 +1,13 @@
+// let arr = [1, 2, 3, 4];
+// let initialValue = {}
+// arr.reduce(function (accumulator, item, index) {
+//   console.log(accumulator, index);
+//   return accumulator;
+// }, initialValue)
+
+let arr = [];
+let initialValue = {}
+arr.reduce(function (accumulator, item, index) {
+  console.log(accumulator, index);
+  return accumulator;
+}, initialValue)
