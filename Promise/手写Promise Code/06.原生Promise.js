492be989921b764ce172008690248e74bf854fd1
unknown <2987642862@qq.com>
2024-11-11 17:16:24 +0800

    Promise.all相关的实现

--- Promise/手写Promise Code/06.原生Promise.js
+++ Promise/手写Promise Code/06.原生Promise.js
@@ -20,22 +20,61 @@
 // }).catch(reason => {
 //     console.log('原生promise reason:', reason);
 // })
-let p1 = new Promise(resolve => {
-    // resolve(22);
-    setTimeout(() => {
-        resolve(22);
-    }, 2000)
-})
-let p2 = new Promise(resolve => {
-    // resolve(333);
-    setTimeout(() => {
-        resolve(333);
-    }, 1000)
-})
+// let p1 = new Promise(resolve => {
+//     // resolve(22);
+//     setTimeout(() => {
+//         resolve(22);
+//     }, 2000)
+// })
+// let p2 = new Promise(resolve => {
+//     // resolve(333);
+//     setTimeout(() => {
+//         resolve(333);
+//     }, 1000)
+// })
 // Promise.all([p1, p2]).then(value => {
 //     console.log('原生value:', value);
 // })
-Promise.race([p1, p2]).then(value => {
-    console.log('原生value:', value);
-})
+// Promise.race([p1, p2]).then(value => {
+//     console.log('原生value:', value);
+// })
 // console.log('原生promise:', p);
+
+Promise.all = function (lists) {
+    return new Promise((resolve, reject) => {
+        let results = [];
+        lists.forEach((promise) => {
+            if (promise instanceof Promise) {
+                promise.then(v => {
+                    results.push(v);
+                    if (results.length === lists.length) {
+                        resolve(results);
+                    }
+                }).catch(err => {
+                    reject(err);
+                })
+            } else {
+                Promise.resolve(promise).then(v => {
+                    results.push(v);
+                    if (results.length === lists.length) {
+                        resolve(results);
+                    }
+                }).catch(err => {
+                    reject(err)
+                })
+            }
+        })
+        // resolve(results);
+    })
+}
+
+// const arr = [Promise.resolve(1), 2];
+// const arr = [Promise.resolve(1)];
+// Promise.all(arr).then(v => console.log('test:', v)); // 输出 [1, 2]
+// arr.push(3);
+Promise.all([Promise.reject(3)])
+    .then(v => console.log('111:', v))
+    .catch(v => console.log('err:', v)); // 输出 3
+// Promise.all([Promise.reject(3)]).catch(v => console.log('err:', v)); // 输出 3
+// Promise.all([1, Promise.resolve(2), Promise.reject(3)]).then(v => console.log(v)).catch(v => console.log('err:',v)); // 输出 3
+//Promise.all([new Promise((res) => setTimeout(() => res(1), 500)), 2, Promise.resolve(3)]).then(v => console.log(v)); // 输出 [1, 2, 3]
